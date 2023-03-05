0edbfca8e71474e52186b4094dff5351b936e8ac
evgenii klimashko <klimashko108@gmail.com>
2023-03-05 22:21:38 +0300

    обавил заполнение графы предметы

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,4 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.hold_browser_open = True
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
