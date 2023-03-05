654039a7cf46fa6be8b5174e9ffc6819fb4a4a51
evgenii klimashko <klimashko108@gmail.com>
2023-03-05 19:52:20 +0300

    тк добавил конфтест файл и в нем есть фикстурв (которая держит браузер открытыми), то в файле test_registration_form.py ввел ф-ю и все действия сложил в нее.

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -3,24 +3,24 @@ from selene import be, have
 import pytest
 import time
 
-def fill_check_reg_form(browser_management):
 
-browser.open('https://demoqa.com/automation-practice-form')
-browser.element('[id="firstName"]').should(be.blank).type('John')
-browser.element('[id="lastName"]').should(be.blank).type('Smith')
-browser.element('[id="userEmail"]').should(be.blank).type('john_smith_980yt304gy@gmail.com')
-browser.element('[for = "gender-radio-1"]').click()
-browser.element('[id="userNumber"]').should(be.blank).type('9079079079')
-time.sleep(5)
-browser.element('[id="dateOfBirthInput"]').send_keys()
-browser.element('[class="react-datepicker__year-select"]').click()
-time.sleep(5)
-browser.element('[value="1986"]').click()
-#value="1986"
-browser.element('[class="react-datepicker__month-select"]').click()
-browser.element('[value="5"]').click()
-browser.element('[aria-label="Choose Sunday, June 15th, 1986"]').click()
-time.sleep(3)
-time.sleep(10)
+def test_reg_form(browser_management):
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element('[id="firstName"]').should(be.blank).type('John')
+    browser.element('[id="lastName"]').should(be.blank).type('Smith')
+    browser.element('[id="userEmail"]').should(be.blank).type('john_smith_980yt304gy@gmail.com')
+    browser.element('[for = "gender-radio-1"]').click()
+    browser.element('[id="userNumber"]').should(be.blank).type('9079079079')
+    time.sleep(5)
+    browser.element('[id="dateOfBirthInput"]').send_keys()
+    browser.element('[class="react-datepicker__year-select"]').click()
+    time.sleep(5)
+    browser.element('[value="1986"]').click()
+    # value="1986"
+    browser.element('[class="react-datepicker__month-select"]').click()
+    browser.element('[value="5"]').click()
+    browser.element('[aria-label="Choose Sunday, June 15th, 1986"]').click()
+    time.sleep(3)
+    time.sleep(10)
 
-#browser.driver.execute_script('window.scrollBy(0, 500)')
+# browser.driver.execute_script('window.scrollBy(0, 500)')
