e05380d5cdd98bb908367be7be7f4b99d30227e6
evgenii klimashko <klimashko108@gmail.com>
2023-03-06 00:49:51 +0300

    Добавил venv

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -30,25 +30,14 @@ def test_reg_form(browser_management):
     time.sleep(3)
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-2"]').click()
-    #time.sleep(3)
-    browser.element('[id="uploadPicture"]').double_click()
-    browser.element('[id="uploadPicture"]').send_keys('C:\Users\klima\PycharmProjects\qa_guru_python_4_5\tests\IMG_20230224_164633_1.jpg')
     time.sleep(3)
-    #<input id="uploadPicture" type="file" lang="en" class="form-control-file">
-    #"C:\Users\klima\OneDrive\Изображения\Saved Pictures\picture.jpg"
+    picture_adress = os.getcwd()+'IMG_20230224_164633_1.jpg'
+    print(picture_adress)
+    #browser.element('[id="uploadPicture"]').double_click()
+    #browser.element('[id="uploadPicture"]').send_keys(os.getcwd()+'IMG_20230224_164633_1.jpg')
+    time.sleep(3)
 
 
-#<input type="checkbox" id="hobbies-checkbox-1" class="custom-control-input" value="1">
-
-#<div class="subjects-auto-complete__control css-yk16xz-control"><div
-#class="subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3"><div
-#class="subjects-auto-complete__placeholder css-1wa3eu0-placeholder"></div><div class="css-1g6gooi"><div
-#class="subjects-auto-complete__input" style="display: inline-block;"><input autocapitalize="none" autocomplete="off" autocorrect="off"
-#id="subjectsInput" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value=""
-#style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;"><div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 16px; font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div></div></div></div><div class="subjects-auto-complete__indicators css-1wy0on6"><span
-#class="subjects-auto-complete__indicator-separator css-1okebmr-indicatorSeparator"></span></div></div>
-
-#<div class="subjects-auto-complete__option subjects-auto-complete__option--is-focused css-1n7v3ny-option"
-#id="react-select-2-option-0" tabindex="-1">Maths</div>
+#"C:\Users\klima\PycharmProjects\qa_guru_python_4_5\tests\IMG_20230224_164633_1.jpg"
 # browser.driver.execute_script('window.scrollBy(0, 500)')
 
